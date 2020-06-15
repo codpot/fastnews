7486e260366d61bd78110a8a5548455810cf524c
Seungwan Seo <wan9824@naver.com>
2020-06-15 20:55:44 +0900

    feat: add urls and views

--- fastnews/urls.py
+++ fastnews/urls.py
@@ -2,5 +2,30 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'', views.index, name='index'),
+    url(r'^$', views.news_list, {'category': 'news'}),
+    url(r'^politics/$', views.news_list, {'category': 'politics'}),
+    url(r'^economy/$', views.news_list, {'category': 'economy'}),
+    url(r'^social/$', views.news_list, {'category': 'social'}),
+    url(r'^life/$', views.news_list, {'category': 'life'}),
+    url(r'^world/$', views.news_list, {'category': 'world'}),
+    url(r'^it/$', views.news_list, {'category': 'it'}),
+    url(r'^news/(?P<id>[0-9]+)$', views.news_detail),
+
+    url(r'^debates/$', views.debates_list),
+    url(r'^debates/(?P<id>[0-9]+)$', views.debates_detail),
+
+    url(r'^notifications/$', views.notifications_list),
+    url(r'^notifications/(?P<id>[0-9]+)$', views.notifications_detail),
+
+    url(r'^likes/$', views.likes_list),
+    url(r'^likes/(?P<id>[0-9]+)$', views.likes_detail),
+
+    url(r'^write/$', views.write),
+
+    url(r'^settlement/$', views.settlement_list),
+    url(r'^settlement/(?P<id>[0-9]+)$', views.settlement_detail),
+
+    url(r'^login/$', views.login),
+    url(r'^register/$', views.register),
+    url(r'^myaccount/$', views.myaccount),
 ]
--- fastnews/views.py
+++ fastnews/views.py
@@ -1,5 +1,57 @@
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse('Hello World!')
+def news_list(request, category):
+    return HttpResponse('News')
+
+
+def news_detail(request, id):
+    return HttpResponse('News')
+
+
+def debates_list(request):
+    return HttpResponse('Debates')
+
+
+def debates_detail(request, id):
+    return HttpResponse('Debates')
+
+
+def notifications_list(request):
+    return HttpResponse('Notifications')
+
+
+def notifications_detail(request, id):
+    return HttpResponse('Notifications')
+
+
+def likes_list(request):
+    return HttpResponse('Likes')
+
+
+def likes_detail(request, id):
+    return HttpResponse('Likes')
+
+
+def write(request):
+    return HttpResponse('Write')
+
+
+def settlement_list(request):
+    return HttpResponse('Settlement')
+
+
+def settlement_detail(request, id):
+    return HttpResponse('Settlement')
+
+
+def login(request):
+    return HttpResponse('Login')
+
+
+def register(request):
+    return HttpResponse('Register')
+
+
+def myaccount(request):
+    return HttpResponse('My Account')
